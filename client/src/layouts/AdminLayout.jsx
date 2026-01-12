71159f1aea747b51fb71e9a0d17384e333e03e91
Quý <quylavip333@gmail.com>
2026-01-12 18:05:32 +0700

    update

---
+++ client/src/layouts/AdminLayout.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import AdminSidebar from "../components/Admin/AdminSidebar.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function AdminLayout() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("token"); // nếu bạn dùng JWT
+        const res = await axios.get(
+          import.meta.env.VITE_API_URL + "/users/me",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setUser(res.data);
+      } catch (error) {
+        console.log("Failed to fetch user", error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <AdminSidebar user={user} />
+      <div className="flex-1 overflow-y-auto">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
